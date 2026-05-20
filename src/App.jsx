e42b52e39e575aa07a40cc5f63630a9faeb2c3da
Ruichen Zheng <ankbzpx@hotmail.com>
2026-05-20 12:25:08 +0300

    update preprint link

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ function App() {
       King Abdullah University of Science and Technology
       <div class="vspacer">
         <span class="link-block">
-          <a href="https://github.com/Ankbzpx/patchwork-experiment/blob/main/preprint/preprint.pdf">
+          <a href="https://arxiv.org/abs/2605.16266">
             <FontAwesomeIcon icon={faFilePdf} />
             <span>Preprint</span>
           </a>
